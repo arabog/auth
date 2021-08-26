6b2eae12ff1a972a4a5be33a239d4f374137420d
iCode_Rx <arababs2015@gmail.com>
2021-08-26 20:39:46 +0100

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,18 @@
+exports.register = (req, res, next) => {
+          res.send("Register Route")
+}
+
+
+exports.login = (req, res, next) => {
+          res.send("Login Route")
+}
+
+
+exports.forgotpassword = (req, res, next) => {
+          res.send("Forgot Password Route")
+}
+
+
+exports.resetpassword = (req, res, next) => {
+          res.send("Reset Password Route")
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+npx create-react-app .
+
+npm i express
+touch .gitignore
+
+npm i dotenv
+npm i --save-dev nodemon
